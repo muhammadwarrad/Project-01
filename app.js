223181e06cd46411f019343955db90b9f250cd19
muhammadwarrad <moe_warrad@yahoo.com>
2022-12-05 21:32:40 -0500

    all done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let secondCard;
 let interval;
 let clicks = 0;
 let add = 0;
+// I made all my cards hidden using display none in css i created a function and used a querySelecor to target each card
 function handleClick(e) {
     clearInterval(interval)
     const cardImg = e.target.querySelector("img")
@@ -18,7 +19,7 @@ function handleClick(e) {
     let click = document.querySelector(".clicks")
 
     click.innerHTML = `clicks: ${clicks}`
-
+// when you start the game the counter is 0 and every time the user clicks on the card it adds one to the counter
     if (counter === 0) {
         firstSelection = card.getAttribute("sports");
         firstCard = cardImg
@@ -41,7 +42,7 @@ function handleClick(e) {
             correctCards[1].classList.add("checked");
             correctCards[1].classList.remove("clicked");
             counter = 0
-
+// the else statement makes it where once counter = 2 it restarts the counter back to 0 (counter max = 2)
 
         } else {
             interval = setInterval(() => {
@@ -54,12 +55,15 @@ function handleClick(e) {
 
         }
     }
+    // if the cards match add 1 
     if (firstSelection === secondSelection) {
         add++;
     }
+    // since there is 6 pairs once adds = 6 display h2 which is "you won"
+
     if (add === 6) {
-    //    bob.style.display="block"
-       alert("you won") 
+        document.getElementById('bob').style.display="block"
+      
     }
 
 }
@@ -71,8 +75,6 @@ cards.forEach((card) => {
 
 
 })
-// close.onclick = function(){
-//     close.style.display = "none"
-// }
+
 
 
